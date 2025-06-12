72763bf205652cec3a6e8803b6f08b17311de3ff
James Howes IV <tuu50210@temple.edu>
2025-06-12 16:06:07 +0900

    Generic sensor header

---
+++ cis1057/tancho/include/tancho/sensor.h
@@ -0,0 +1,11 @@
+/***************************************
+* sensor.h                             *
+* Generic sensor header                *
+***************************************/
+
+typedef struct {
+    uint32_t timestamp_ms;  // System timestamp
+    uint16_t raw_value;     // 12-bit ADC output (0-4095)
+    uint8_t  channel;       // Which channel this came from
+    uint8_t  flags;         // Data validity indicators
+} SensorReading;
