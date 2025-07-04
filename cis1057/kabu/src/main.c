08b442dbe582600386b7a600796133c2c51fe5cb
Jim Howes <tuu50210@tuj.temple.edu>
2025-07-04 12:26:34 +0900

    Updated main to use allocator

--- cis1057/kabu/src/main.c
+++ cis1057/kabu/src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Allocator.h"
+
+Allocator *alloc;
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    alloc = Allocator_new();
+    int *num = Allocator_push(alloc, sizeof(int));
+    int *num2 = Allocator_push(alloc, sizeof(int));
+    *num = 5;
+    *num2 = 10;
+    printf("num = %d, num2 = %d\n", *num, *num2);
+    free(alloc);
+    return EXIT_SUCCESS;
 }
