511bff41a7f24e871d6410cc441fb5f144de8a0c
Jim Howes <tuu50210@tuj.temple.edu>
2025-07-07 07:59:01 +0900

    Project Euler problems 1-3

---
+++ cis1057/euler/euler3.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * PROJECT EULER #3
+ * Largest Prime Factor
+ *
+ * The prime factors of 13195 are 5, 7, 13, and 29.
+ *
+ * What is the largest prime factor of the number 600851475143?
+ */
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
