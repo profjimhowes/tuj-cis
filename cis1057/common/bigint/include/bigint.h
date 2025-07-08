87130c473d2be863e151d8154f3c06c82afd9557
Jim Howes <tuu50210@tuj.temple.edu>
2025-07-08 09:31:58 +0900

    Updated API

--- cis1057/common/bigint/include/bigint.h
+++ cis1057/common/bigint/include/bigint.h
@@ -1,6 +1,23 @@
 typedef struct {
-    size_t bytes;
-    int value[];
-} BigInt;
+    struct {
+        BigInt* (*from_int)(long value);
+        BigInt* (*from_str)(const char* str);
+        BigInt* (*copy)(const BigInt* src);
+        void (*destroy)(BigInt* bi);
+    } create;
+    
+    struct {
+        BigInt* (*add)(const BigInt* a, const BigInt* b);
+        BigInt* (*sub)(const BigInt* a, const BigInt* b);
+        BigInt* (*mul)(const BigInt* a, const BigInt* b);
+        BigInt* (*div)(const BigInt* a, const BigInt* b);
+    } math;
+    
+    struct {
+        int (*cmp)(const BigInt* a, const BigInt* b);
+        bool (*eq)(const BigInt* a, const BigInt* b);
+        bool (*lt)(const BigInt* a, const BigInt* b);
+    } compare;
+} BigIntLib;
 
-void BigInt_add(BigInt *x, BigInt *y);
+extern const BigIntLib BigInt;
--- cis1057/common/bigint/src/bigint.c
+++ cis1057/common/bigint/src/bigint.c
@@ -1,1 +1,10 @@
 #include "bigint.h"
+
+struct bigint {
+    size_t bytes;
+
+}
+
+BigInt_t *bigint_add(BigInt_t *a, const BigInt_t *b) {
+    return;
+}
