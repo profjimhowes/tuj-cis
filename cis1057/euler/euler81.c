a8d415908649ea64ae277d3014b86f5b35c7f63e
Jim Howes <tuu50210@tuj.temple.edu>
2025-07-11 07:47:16 +0900

    PE#81, more file operations

---
+++ cis1057/euler/euler81.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * PROJECT EULER #81
+ * Path Sum: Two Ways
+ *
+ * In the 5 by 5 matrix below, the minimal path sum from the top left to the
+ * bottom right, by only moving to the right and down, is indicated by
+ * parentheses and is equal to 2427.
+ *
+ *     (131) 673  234  103   18
+ *     (201)( 96)(342) 965  150
+ *      630  803 (746)(422) 111
+ *      537  699  497 (121) 956
+ *      805  732  524 ( 37)(331)
+ *
+ * Find the minimal path sum from the top left to the bottom right by only
+ * moving right and down in data/euler81.txt, a 31Kb text file containing an
+ * 80 by 80 matrix.
+ */
+
+#define INPUT_FILE "data/euler81.txt"
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
