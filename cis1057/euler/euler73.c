1719ebeb2585cbac179602f8c8ccabb6057cefd6
Jim Howes <tuu50210@tuj.temple.edu>
2025-07-15 16:50:28 +0900

    PE harder problems

---
+++ cis1057/euler/euler73.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * PROJECT EULER #73
+ * Counting Fractions in a Range
+ *
+ * Consider the fraction, n/d, where n and d are positive integers. If n < d
+ * and GCF(n,d) = 1, it is called a reduced proper fraction.
+ *
+ * If we list the set of reduced proper fractions for d ≤ 8 in ascending order
+ * of size, we get:
+ *
+ * 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, ... , 7/8
+ *
+ * It can be seen that there are 3 fractions between 1/3 and 1/2.
+ *
+ * How many fractions lie between 1/3 and 1/2 in the sorted set of reduced
+ * proper fractions for d ≤ 12000?
+ */
+
+#define D 12000
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
